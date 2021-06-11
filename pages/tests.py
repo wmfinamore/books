3918ec8737cd7ab681c6f1121b723a2d20cb9c8b
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-06-10 22:29:02 -0300

    Implemented test to resolve the url checking the related view

--- pages/tests.py
+++ pages/tests.py
@@ -1,5 +1,6 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from .views import HomePageView
 
 
 class HomepageTests(SimpleTestCase):
@@ -22,3 +23,10 @@ class HomepageTests(SimpleTestCase):
 
     def test_homepage_does_not_contain_incorrect_html(self):
         self.assertContains(self.response, 'Hi there! I should not ben the home.')
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
