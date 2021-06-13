d28693ce8079436494b5a08ebcf3e5bc03b3b51f
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-06-13 17:59:22 -0300

    Added explicitly the static finders

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATIC_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
