be0affec8167cccef4b2646caa067b0b4b04fd26
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-06-13 17:40:49 -0300

    Added static file location for production environment

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
