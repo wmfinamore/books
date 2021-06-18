42d97b9133744cb1d79f5178395e5162c6b7e829
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-06-17 21:12:41 -0300

    Configured the email backend console

--- config/settings.py
+++ config/settings.py
@@ -165,3 +165,5 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
