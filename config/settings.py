2d98532ab46f822186360cbe821e3ec654d101ed
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-06-19 23:00:15 -0300

    Included comments about use of environs package

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+# example using environs package
+# SECRET_KEY = env("DJANGO_SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
