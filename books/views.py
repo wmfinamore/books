f2c972e0ce28ffe78ddd57706a59b3a0a40d0346
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-06-29 22:36:45 -0300

    Refactored search result list view to use value from URL

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import (
+ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin
     )
@@ -26,9 +26,9 @@ class SearchResultsListView(ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/search_results.html'
-    # queryset = Book.objects.filter(title__icontains='beginners')
 
     def get_queryset(self):
+        query = self.request.GET.get('q')
         return Book.objects.filter(
-            Q(title__icontains='django') | Q(title__icontains='professionals')
+            Q(title__icontains=query) | Q(author__icontains=query)
         )
