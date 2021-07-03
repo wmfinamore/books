90f5b59b5fc1f0992230bd84f20154c9eb1fb715
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-03 16:01:19 -0300

    Adjusted configurations to user environment variables

--- config/settings.py
+++ config/settings.py
@@ -25,14 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
 
 # example using environs package
-# SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
-# DEBUG = env.bool("DJANGO_DEBUG")
+# DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = env.bool("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
