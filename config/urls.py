1ccaeacd8bd108598746746bec654d17dd38e3ab
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-03 16:53:05 -0300

    Changed the admin url for security reasons

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     # Django Admin
-    path('admin/', admin.site.urls),
+    path('anything-but-admin/', admin.site.urls),
 
     # User management
     path('accounts/', include('allauth.urls')),
